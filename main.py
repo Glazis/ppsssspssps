e72dbf3a49034b16428b3198256cf608abd165e9
Glazis <13vitalya2004@gmail.com>
2021-09-25 14:47:26 +1000

    2

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-print('x','y','z','w','f')
-for x in range (2):
-    for y in range (2):
-        for z in range (2):
-            for w in range (2):
-                f= not (x and y) or (z and w) or (w and y)or (not x and w)
-                if f==0:
-                    print(x,y,z,w,f)
+import time 
+from tkinter import Tk, Label
+from tkinter.constants import E 
+
+root = Tk()
+root.title("часы")
+root.geometry('+800-500')
+root.overrideredirect (True)
+Lb = Label(root, bg= 'black',fg= 'white', text= 'я учу питон ', font= 'arial  90')
+Lb.pack(ipadx=20, ipady=20)
+
+
+def gettiem():
+    p=':'if int (time.strftime('%S')) % 2 == 0 else ' '
+    Lb['text'] = time.strftime(f'%H{p}%M{p}%S')
+    Lb.after(1000,gettiem)
+#label это наклейка то что отображается на экране
+
+def closing(E):
+    root.destroy()
+
+Lb.after_idle(gettiem)
+root.bind('<Escape>',closing)
+
+root.mainloop()
